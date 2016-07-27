2fc6dba008c52a49386d0ce736aeedcfc52bda57
plasticteaspoon <jessica@drurys.org>
2016-07-27 17:48:04 +0100

    First commit. Made the home page look good!

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/wwwroot'));
+
+app.listen(80, function () {
+    console.log('Server started');
+});
