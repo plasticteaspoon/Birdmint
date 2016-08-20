e2f215c77af9574d54bec4ad73658cda97f2eb14
plasticteaspoon <jessica@drurys.org>
2016-08-20 14:23:16 +0100

    Made the book site ready to go live

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var app = express();
 
 app.use(express.static(__dirname + '/wwwroot'));
 
+app.get('/', function (request, response) {
+    response.redirect('/home.html');
+});
+
 books.addRoutes(app);
 
 app.listen(80, function () {
