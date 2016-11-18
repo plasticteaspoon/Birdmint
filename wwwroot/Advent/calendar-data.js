acdea0cfc69b8ba4741de4fc40f729b9174fa671
plasticteaspoon <jessica@drurys.org>
2016-11-18 17:09:01 +0000

    Added an advent calendar. Got most of it finished but stiil needs some pages.

---
+++ wwwroot/Advent/calendar-data.js
@@ -0,0 +1,199 @@
+var calendarData = {
+        rows : [
+            {
+                cols : [
+                    {
+                        number : 1,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/long-haired-girl.jpg',
+                        open : false,
+                        link : '/day1'
+                    }, 
+                    {
+                        number : 2,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/side-view-girl-coloured.png',
+                        open : false,
+                        link : '/day2'
+                    }, 
+                    {
+                        number : 3,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/anime-eye.png',
+                        open : false,
+                        link : '/day3'
+                    }, 
+                    {
+                        number : 4,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : "pictures/small/sheeta's-crystal.png",
+                        open : false,
+                        link : '/day4'
+                    }, 
+                    {
+                        number : 5,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/green-girl.png',
+                        open : false,
+                        link : '/day5'
+                    }, 
+                ]
+            }, 
+            {
+                cols : [
+                    {
+                        number : 6,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/cat.jpg',
+                        open : false,
+                        link : '/day6'
+                    }, 
+                    {
+                        number : 7,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/angry-sad-anime-face.png',
+                        open : false,
+                        link : '/day7'
+                    }, 
+                    {
+                        number : 8,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/full-glass.png',
+                        open : false,
+                        link : '/day8'
+                    }, 
+                    {
+                        number : 9,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/girl-explosion.png',
+                        open : false,
+                        link : '/day9'
+                    }, 
+                    {
+                        number : 10,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/purple-eyed-boy.png',
+                        open : false,
+                        link : '/day10'
+                    }, 
+                ]
+            }, 
+            {
+                cols : [
+                    {
+                        number : 11,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/flower.jpg',
+                        open : false,
+                        link : '/day11'
+                    }, 
+                    {
+                        number : 12,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/anime-princess.png',
+                        open : false,
+                        link : '/day12'
+                    }, 
+                    {
+                        number : 13,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/anime-princess.png',
+                        open : false,
+                        link : '/day13'
+                    }, 
+                    {
+                        number : 14,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/anime-princess.png',
+                        open : false,
+                        link : '/day14'
+                    }, 
+                    {
+                        number : 15,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/anime-princess.png',
+                        open : false,
+                        link : '/day15'
+                    }, 
+                ]
+            }, 
+            {
+                cols : [
+                    {
+                        number : 16,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/anime-princess.png',
+                        open : false,
+                        link : '/day16'
+                    }, 
+                    {
+                        number : 17,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/anime-princess.png',
+                        open : false,
+                        link : '/day17'
+                    }, 
+                    {
+                        number : 18,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/anime-princess.png',
+                        open : false,
+                        link : '/day18'
+                    }, 
+                    {
+                        number : 19,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/anime-princess.png',
+                        open : false,
+                        link : '/day19'
+                    }, 
+                    {
+                        number : 20,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/anime-princess.png',
+                        open : false,
+                        link : '/day20'
+                    }, 
+                ]
+            }, 
+            {
+                cols : [
+                    {
+                        number : 21,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/anime-princess.png',
+                        open : false,
+                        link : '/day21'
+                    }, 
+                    {
+                        number : 22,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/anime-princess.png',
+                        open : false,
+                        link : '/day22'
+                    }, 
+                    {
+                        number : 23,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/anime-princess.png',
+                        open : false,
+                        link : '/day23'
+                    }, 
+                    {
+                        number : 24,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/anime-princess.png',
+                        open : false,
+                        link : '/day24'
+                    }, 
+                    {
+                        number : 25,
+                        coverImgUrl : 'pictures/small/robot-head.png',
+                        imgUrl : 'pictures/small/anime-princess.png',
+                        open : false,
+                        link : '/day25'
+                    }, 
+                ]
+            }, 
+        ]
+    }
---
+++ wwwroot/Advent/model.js
@@ -0,0 +1,115 @@
+var adventApp = angular.module("adventApp", ["ngRoute"]);
+
+adventApp.controller("adventController", ["$scope", "$location", function (scope, location) {
+    scope.messages = {
+        warning:'',
+        success:'',
+        error:'',
+        clear: function () {
+            this.warning = '';
+            this.success = '';
+            this.error = '';
+        }
+    };
+
+    scope.calendar = calendarData;
+
+    scope.openDoor = function (col) {
+        var december = 10;
+        var doorDate = new Date(2016, december, col.number, 0, 0, 0);
+        var now = new Date();
+
+        if(now > doorDate) {
+            col.open = true;
+        }
+    };
+
+    scope.dayPage = function (col) {
+        location.url(col.link);
+    };
+}]);
+
+adventApp.config(["$routeProvider", function(routeProvider) {
+    routeProvider
+    .otherwise( {
+        templateUrl : "calendar-view.html"
+    })
+    .when("/day1", {
+        templateUrl : "templates/day1.html"
+    })
+    .when("/day2", {
+        templateUrl : "templates/day2.html"
+    })
+    .when("/day3", {
+        templateUrl : "templates/day3.html"
+    })
+    .when("/day4", {
+        templateUrl : "templates/day4.html"
+    })
+    .when("/day5", {
+        templateUrl : "templates/day5.html"
+    })
+    .when("/day6", {
+        templateUrl : "templates/day6.html"
+    })
+    .when("/day7", {
+        templateUrl : "templates/day7.html"
+    })
+    .when("/day8", {
+        templateUrl : "templates/day8.html"
+    })
+    .when("/day9", {
+        templateUrl : "templates/day9.html"
+    })
+    .when("/day9", {
+        templateUrl : "templates/day9.html"
+    })
+    .when("/day10", {
+        templateUrl : "templates/day10.html"
+    })
+    .when("/day11", {
+        templateUrl : "templates/day11.html"
+    })
+    .when("/day12", {
+        templateUrl : "templates/day12.html"
+    })
+    .when("/day13", {
+        templateUrl : "templates/day13.html"
+    })
+    .when("/day14", {
+        templateUrl : "templates/day14.html"
+    })
+    .when("/day15", {
+        templateUrl : "templates/day15.html"
+    })
+    .when("/day16", {
+        templateUrl : "templates/day16.html"
+    })
+    .when("/day17", {
+        templateUrl : "templates/day17.html"
+    })
+    .when("/day18", {
+        templateUrl : "templates/day18.html"
+    })
+    .when("/day19", {
+        templateUrl : "templates/day19.html"
+    })
+    .when("/day20", {
+        templateUrl : "templates/day20.html"
+    })
+    .when("/day21", {
+        templateUrl : "templates/day21.html"
+    })
+    .when("/day22", {
+        templateUrl : "templates/day22.html"
+    })
+    .when("/day23", {
+        templateUrl : "templates/day23.html"
+    })
+    .when("/day24", {
+        templateUrl : "templates/day24.html"
+    })
+    .when("/day25", {
+        templateUrl : "templates/day25.html"
+    });
+}]);
