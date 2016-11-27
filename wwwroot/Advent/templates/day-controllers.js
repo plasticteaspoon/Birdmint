0aaf35b088a9a3503d44853c1bdb3c48e7826a1d
plasticteaspoon <jessica@drurys.org>
2016-11-27 10:00:12 +0000

    Added a page for day 5 and did a little to day 24.

--- wwwroot/Advent/templates/day-controllers.js
+++ wwwroot/Advent/templates/day-controllers.js
@@ -1,7 +1,25 @@
 adventApp.controller("day2Controller", ["$scope", function (scope) {
+    scope.messages = {
+        info:'',
+        success:'',
+        error:'',
+        clear: function () {
+            this.info = '';
+            this.success = '';
+            this.error = '';
+        }
+    };
+
+    var december = 10;
+    var pageDate = new Date(2016, december, 2, 0, 0, 0);
+    var now = new Date(2016, december);
     scope.showAnswer = false;
 
     scope.giveAnswer = function () {
-        scope.showAnswer = true;
+        if(now > pageDate) {
+            scope.showAnswer = true;
+        } else {
+            scope.messages.info = "The answer shall only be revealed one day after the riddle has been revealed. You must be patient.";
+        }
     }
 }]);
