c89c49e811cf8d87b89b633c5f45b5c88071f919
plasticteaspoon <jessica@drurys.org>
2016-12-17 11:28:32 +0000

    reated

---
+++ wwwroot/advent/story-directive.js
@@ -0,0 +1,18 @@
+angular.module("adventApp").directive("jdStory", [function () {
+    return {
+        template: '<div>	' +
+                    '	<p class=MsoNormal>The sun hovered in a sky devoid of clouds. Its rays beat	down on the cracked earth sucking any moisture it still had away from it.	Hidden in the shadow of some cliffs a figure stood looking for something.</p>	' +
+                    '	<p class=MsoNormal>It was a woman who might have once been pretty though years	of living in the wasteland of Green Lake had taken its toll on her. She wore a	cowboy hat and, on her feet, were a pair of cowboy boots studded with turquoise	stones. At her hip rested a gun, in a pocket of her jacket lay a tube of	lipstick.</p>	' +
+                    '	<p class=MsoNormal>This woman was known far and wide. If you went to the town	nearby you&#39;d see a poster saying something like this.</p>	' +
+                    '	<p class=MsoNormal>WANTED</p>	' +
+                    '	<p class=MsoNormal>Kissin&#39; Kate Barlow</p>	' +
+                    '	<p class=MsoNormal>&nbsp;</p>	' +
+                    '	<p class=MsoNormal>Reward: $10,000</p>	' +
+                    '	<p class=MsoNormal>DEAD or ALIVE</p>	' +
+                    '	<p class=MsoNormal>Kissin&#39; Kate Barlow was an outlaw. She had been ever since	she killed the sheriff for killing Sam.<i>Oh, Sam.</i> Kate thought with a	slightly wistful look in her eyes, then her face hardened and she set off along	the edge of the cliff. Slowly, its protection slipped away, as if angry about	what she&#39;d done to the lake. Kate walked until she was in sight of the road.	Now all she had to do was wait.</p>	' +
+                    '	<p class=MsoNormal>Finally, after what seemed forever, a carriage appeared in	the distance.<i>Not up to date with the new motor car.</i> Kate noticed.	Swiftly, as it passed her, she swung herself up onto the back and landed like a	cat, making very little noise so she could observe the occupants without them	panicking. It looked to be a couple of about 20 years of age.<i>Perfect,</i>she	thought, her eyes gleaming.<i>He&#39;ll probably have bought her an expensive	Christmas present. </i>She considered the situation. <i>The direct approach is	most likely the best option. He looks like one who plays tough but squeals like	a pig at any hint of real danger. </i>Kate decided. Then, again cat-like in her	movements, forced the driver to stop the carriage and confronted the pair.</p>	' +
+                    '	<p class=MsoNormal>Everything had gone smoothly and her pocket now contained a	box wrapped in paper and, so she was told holding a diamond bracelet.	Annoyingly the ride had taken her almost to town and she would have to acquire	suitable transport for the return journey. A voice shouted behind her and she	whipped around. &quot;Hey! You! Where do you think you&#39;re going? I&#39;ve heard there&#39;s	a cell in prison with your name on it. Isn&#39;t that right Miss Barlow.&quot; It was a	police officer and with him was the cab driver.<i>Damn! He must have broken	free from the ropes I put on him.</i>She ran into town and the officer	followed her, blowing his whistle to alert everyone to her presence.</p>' +
+                    '</div>',
+        replace: true
+    };
+}]);
