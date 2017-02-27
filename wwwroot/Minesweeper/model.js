35bd8ab550af7c73f27425d2b6475f34c8890f97
plasticteaspoon <jessica@drurys.org>
2017-02-27 22:18:47 +0000

    created working version of game.

--- wwwroot/Minesweeper/model.js
+++ wwwroot/Minesweeper/model.js
@@ -16,9 +16,9 @@ mineApp.controller("mineController", ["$scope", function (scope) {
     var minesPlaced = 0;
 
     scope.grid = {
-        numRows: 4,
-        numCols: 4,
-        numMines: 6,
+        numRows: 10,
+        numCols: 10,
+        numMines: 10,
         rows: []
     };
 
@@ -28,7 +28,15 @@ mineApp.controller("mineController", ["$scope", function (scope) {
         };
 
         for (var j = 0; j < scope.grid.numCols; j++) {
-            row.cells.push({ mined: false, open: false, marked: false, x: j, y: i, numCloseMines: 0});
+            row.cells.push({ 
+                mined: false,
+                detonated: false, 
+                open: false, 
+                marked: false, 
+                x: j, 
+                y: i, 
+                numCloseMines: 0
+            });
         }
 
         scope.grid.rows.push(row);
@@ -38,24 +46,32 @@ mineApp.controller("mineController", ["$scope", function (scope) {
     countSurroundingMines();
 
     scope.cellClicked = function (cell) {
-        if(cell.open == false) {
-            cell.open = true;
-        } else {
-            cell.open = false;
+        if(getGameState() == 'playing') {
+            if(cell.marked) {
+                //do nothing
+            } else if(cell.mined) {
+                cell.detonated = true;
+                gameOver();
+            } else {
+                openCell(cell);
+            }
         }
+
     };
     scope.cellRightClicked = function (cell) {
-        if(cell.marked == false) {
-            cell.marked = true;
-        } else {
-            cell.marked = false;
+        if(getGameState() == 'playing') {
+            if(cell.marked == false && cell.open == false) {
+                cell.marked = true;
+            } else {
+                cell.marked = false;
+            }
         }
     };
 
     function placeMines () {
         while (minesPlaced < scope.grid.numMines) {
-            var randomRow = Math.floor((Math.random() * (scope.grid.numRows - 1)) + 0);
-            var randomCol = Math.floor((Math.random() * (scope.grid.numCols - 1)) + 0);
+            var randomRow = Math.floor((Math.random() * scope.grid.numRows));
+            var randomCol = Math.floor((Math.random() * scope.grid.numCols));
 
             if (scope.grid.rows[randomRow].cells[randomCol].mined == false) {
                 scope.grid.rows[randomRow].cells[randomCol].mined = true;
@@ -65,43 +81,106 @@ mineApp.controller("mineController", ["$scope", function (scope) {
     }
 
     function countSurroundingMines () {
-        for (var i = 0; i < scope.grid.numRows; i++) {
+        var cell;
 
-            for (var j = 0; j < scope.grid.numCols; j++) {
+        for (var j = 0; j < scope.grid.numRows; j++) {
+            for (var i = 0; i < scope.grid.numCols; i++) {
                 var mineCount = 0;
+                cell = getCell(j, i);
+                var neighbors = getNeighbors(cell);
+
+                neighbors.forEach(function (neighbor) {
+                    if(neighbor.mined) {
+                        mineCount++;
+                    }
+                });
                 
-                if(scope.grid.rows[i].cells[j-1] && 
-                scope.grid.rows[i].cells[j-1].mined == true) {
-                       mineCount ++;
-                } if(scope.grid.rows[i].cells[j+1] &&
-                  scope.grid.rows[i].cells[j+1].mined == true) {
-                      mineCount ++;
-                } if(scope.grid.rows[i-1] && 
-                  scope.grid.rows[i-1].cells[j].mined == true) {
-                      mineCount ++;
-                } if(scope.grid.rows[i-1] &&
-                  scope.grid.rows[i-1].cells[j-1] && 
-                  scope.grid.rows[i-1].cells[j-1].mined == true) {
-                      mineCount ++;
-                } if(scope.grid.rows[i-1] &&
-                  scope.grid.rows[i-1].cells[j+1] && 
-                  scope.grid.rows[i-1].cells[j+1].mined == true) {
-                      mineCount ++;
-                } if(scope.grid.rows[i+1] && 
-                  scope.grid.rows[i+1].cells[j].mined == true) {
-                      mineCount ++;
-                } if(scope.grid.rows[i+1] &&
-                  scope.grid.rows[i+1].cells[j-1] && 
-                  scope.grid.rows[i+1].cells[j-1].mined == true) {
-                      mineCount ++;
-                } if(scope.grid.rows[i+1] &&
-                  scope.grid.rows[i+1].cells[j+1] && 
-                  scope.grid.rows[i+1].cells[j+1].mined == true) {
-                      mineCount ++;
-                }
-                
-                scope.grid.rows[i].cells[j].numCloseMines = mineCount;
+                cell.numCloseMines = mineCount;
             }
         }
+    }
+
+    function openCell (cell) {
+        if(cell.open) {
+            return;
+        }
+
+        var neighbors = getNeighbors(cell);
+        cell.open = true;
+
+        if(cell.numCloseMines == 0) {
+            neighbors.forEach(function (neighbor) {
+                openCell(neighbor);
+            });
+        }
+    }
+
+    function getCell (rowNum, colNum) {
+        var result = null;
+
+        if (scope.grid.rows[rowNum] && scope.grid.rows[rowNum].cells[colNum]) {
+            result = scope.grid.rows[rowNum].cells[colNum];
+        }
+        return result;
+    }
+
+    function getNeighbors (cell) {
+        var neighbors = [];
+        var neighbor;
+
+        for(var i=-1; i<=1; i++) {
+            for(var j=-1; j<=1; j++) {
+                neighbor = getCell(cell.y + i, cell.x + j);
+                //a cell isn't its own neighbor. REMEMBER THIS!!!
+                //either in a different collum or a different row. Or both.
+                if(neighbor && (cell.y != i || cell.x != j)) {
+                    neighbors.push(neighbor);
+                }
+            }
+        }
+
+        return neighbors;        
+    }
+
+    function getGameState () {
+        var openCellCount = 0;
+        var markedCellCount = 0;
+        var explodedCellCount = 0;
+
+        scope.grid.rows.forEach(function (row) {
+            row.cells.forEach(function (cell) {
+                if(cell.open && cell.mined) {
+                    explodedCellCount++;
+                }
+                if(cell.open) {
+                    openCellCount++;
+                }
+                if(cell.marked) {
+                    markedCellCount++;
+                }
+            });
+        });
+
+        if(explodedCellCount > 0) {
+            return 'lost';
+        } else if(openCellCount + markedCellCount == scope.grid.numRows*scope.grid.numCols && 
+                  markedCellCount == scope.grid.numMines) {
+            return 'won';
+        } else {
+            return 'playing';
+        }
+    }
+
+    scope.getGameState = getGameState;
+
+    function gameOver () {
+        scope.grid.rows.forEach(function (row) {
+            row.cells.forEach(function (cell) {
+                if(cell.mined) {
+                    cell.open = true;
+                }
+            });
+        });
+
     }
 }]);
