66aa077008ed81a57d9148aef34799f43451b4ec
plasticteaspoon <jessica@drurys.org>
2017-04-17 20:32:47 +0100

    Got a kitten to move across the screen and fixed explosion.

--- wwwroot/Opas-birthday/model.js
+++ wwwroot/Opas-birthday/model.js
@@ -13,12 +13,14 @@ opaApp.controller("opaController", ["$scope", "$interval", function (scope, inte
     };
 
     scope.countdown = 10;
+    scope.bombExploded = false;
 
     scope.startCountdown = function () {
         var timerId = interval(function () {
             scope.countdown = scope.countdown-1;
             if(scope.countdown == 0) {
                 interval.cancel(timerId);
+                scope.bombExploded = true;
             }
         }, 1000);
     };
@@ -30,4 +32,25 @@ opaApp.controller("opaController", ["$scope", "$interval", function (scope, inte
             return true;
         }
     };
+
+    //kitten starting position
+    scope.style = {
+            'height': '50px',
+            'top': '200px',
+            'left': -70,
+            'position': 'absolute',
+            'display': 'block',
+            'z-index': '-1'
+    };
+
+    var movePerson = function () {
+        var timerId = interval(function () {
+            if(scope.style.left < 700) {
+                scope.style.left++;
+            }  else {
+                interval.cancel(timerId);
+            }
+        }, 20);
+    };
+    setTimeout(movePerson(), 1000);
 }]);
